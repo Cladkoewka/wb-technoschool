687b8f5c148f9497ffc0dc9c8cfce42cd316f4cf
Cladkoewka <codewarriorms@gmail.com>
2025-08-10 16:31:43 +0300

    l2.8 changed

--- level2/task8/main.go
+++ level2/task8/main.go
@@ -11,12 +11,13 @@ func main() {
 	PrintTime()
 }
 
+// PrintTime output current time to std at format "Time: 15:04:05"
 func PrintTime() {
 	timeNow, err := ntp.Time("pool.ntp.org")
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Ошибка получения: ", err)
+		fmt.Fprintln(os.Stderr, "Error: ", err)
 		os.Exit(1)
 	}
 
-	fmt.Println("Время:", timeNow.Format("15:04:05"))
+	fmt.Println("Time:", timeNow.Format("15:04:05"))
 }
