f1c0114b9b70898db945573de6c961e11e2cbc02
Cladkoewka <codewarriorms@gmail.com>
2025-08-31 18:03:39 +0300

    task 17 completed

---
+++ level2/task17/go.mod
@@ -0,0 +1,3 @@
+module github.com/Cladkoewka/wb-technoschool/level2/task17
+
+go 1.24.2
---
+++ level2/task17/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	// cmd params
+	timeout := flag.Duration("timeout", 10*time.Second, "connection timeout")
+	flag.Parse()
+	
+	if flag.NArg() < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s [--timeout duration] host port\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+	address := net.JoinHostPort(host, port)
+
+	// connection to server
+	conn, err := net.DialTimeout("tcp", address, *timeout)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Connection failed: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Fprintf(os.Stderr, "Connected to %s\n", address)
+
+	// done chan
+	done := make(chan struct{})
+
+	// read from socket -> STDOUT
+	go func() {
+		scanner := bufio.NewScanner(conn)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintf(os.Stderr, "Read error: %v\n", err)
+		}
+		close(done)
+	}()
+
+	// read from STDIN -> socket
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			text := scanner.Text()
+			_, err := fmt.Fprintln(conn, text)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Write error: %v\n", err)
+				break
+			}
+		}
+		// if stdin closed (Ctrl+D) — close connection
+		conn.Close()
+	}()
+
+	// Wait while done
+	<-done
+	fmt.Fprintln(os.Stderr, "Connection closed")
+}
